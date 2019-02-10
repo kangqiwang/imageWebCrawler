571b5d0f420011666d59d8a54059ae156f8da39a
kangqiwang <kq19921207@gmail.com>
2019-02-10 01:47:56 +0000

    update version

--- python/imageSimilarity.py
+++ python/imageSimilarity.py
@@ -1,13 +1,15 @@
 import cv2
-import urllib
+import urllib.request
 import numpy as np
+from matplotlib import pyplot as plt
+
 # import pandas as pd
 
 # csv_file = pd.read_csv('opportunity.csv')
 # print(csv_file[,6])
 
 def url_to_image(url):
-    resp=urllib.urlopen(url)
+    resp=urllib.request.urlopen(url)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     return image
@@ -22,39 +24,19 @@ image1 = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
 image2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
 
 # Initiate SIFT detector
-sift=cv2.SIFT()
+orb=cv2.ORB_create()
 
 #find the keypoints and descriptors with SIFT
-kp1, des1 =sift.detectAndCompute(image1,None)
-kp2, des2 =sift.detectAndCompute(image2,None)
+kp1, des1 =orb.detectAndCompute(image1,None)
+kp2, des2 =orb.detectAndCompute(image2,None)
 
-# BFMatcher with default params
-bf =cv2.BFMatcher()
-matches= bf.knnMatch(des1,des2,k=2)
 
-# apply ratio test
-good =[]
-for m,n in matches:
-    if(m.distance < 0.75*n.distance):
-        good.append([m])
-
-# cv.drawMatchesKnn expects list of lists as matches
-
-img3= cv2.drawMatchesKnn(image1,kp1,image2,kp2,good,flags=2)
-
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+# Match descriptors.
+matches = bf.match(des1,des2)
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+# Draw first 10 matches.
+img3 = cv2.drawMatches(image1,kp1,image2,kp2,matches,None, flags=2)
 plt.imshow(img3),plt.show()
-
-
-# kp1,des1 = sift.detectAndCompute(image1,None)
-# kp2,des2 = sift.detectAndCompute(image2,None)
-# bf = cv2.BFMatcher()
-# matches=bf.match(des1,des2)
-# matches=sorted(matches, key=lambda val: val.distance)
-# img3= drawMatches(img1,kp1,img2,kp2,matches[:25])
-
-# #img1= cv2.drawKeypoints(image1,kp,outImage=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-# cv2.imshow("Image1", img3)
-# #cv2.imshow("Image2", image2)
-# cv2.waitKey(0)
-# cv2.destoryWindow('Matched Features')
