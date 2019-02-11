a182d98fce089e9453225b34002ad3e25bc6c9db
kangqiwang <kq19921207@gmail.com>
2019-02-11 01:52:48 +0000

    update similarity scroc

--- python/imageSimilarity.py
+++ python/imageSimilarity.py
@@ -2,11 +2,11 @@ import cv2
 import urllib.request
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
-# import pandas as pd
-
-# csv_file = pd.read_csv('opportunity.csv')
-# print(csv_file[,6])
+def open_csv():
+    df=pd.read_csv('python/opportunity.csv', usecols=['supplier.image','amazon.image'])
+    return df
 
 def url_to_image(url):
     resp=urllib.request.urlopen(url)
@@ -14,29 +14,49 @@ def url_to_image(url):
     image = cv2.imdecode(image, cv2.IMREAD_COLOR)
     return image
 
-url1 = "http://ecx.images-amazon.com/images/I/416aMYxRqVL._SL220_.jpg"
-url2 = "https://d8mkdcmng3.imgix.net/750c/pc-and-video-games-accessories-ps4-ps4-controllers-nacon-revolution-pro-controller-v2-rig-limited-edition-for-ps4.jpg?bg=0FFF&fit=fill&h=176&q=90&w=176&s=3eb6421e9dd11be175031ef57d840f7a"
+def similarity(matches,kp2):
+    good = []
+    percent=0
+    for m,n in matches:
+        if m.distance < 0.90*n.distance:
+            good.append([m])
+    percent=len(good)/len(kp2)
+    return percent
 
-image1 =url_to_image(url1)
-image2 =url_to_image(url2)
+def main():
+    similarity_result=[]
+    df=open_csv()
+    for index, row in df.iterrows():
+        try:
+            image1 = url_to_image(row['supplier.image'])
+            image2 = url_to_image(row['amazon.image'])
+        except:
+            print('error 404')
+            image1=None
+            image2=None
+        # print('image1 this is:          ',image2)
+        # print('image2 this is:          ',image1)
+        if((image1 is not None) and (image2 is not None) and (len(image1)!=0) and (len(image2)!=0)):
+            image1 = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
+            image2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
+            
+            # Initiate SIFT detector
+            orb=cv2.ORB_create()
+            #find the keypoints and descriptors with SIFT
+            kp1, des1 =orb.detectAndCompute(image1,None)
+            kp2, des2 =orb.detectAndCompute(image2,None)
+            # Sort them in the order of their distance.
+            bf = cv2.BFMatcher()
+            matches = bf.knnMatch(des1,des2, k=2)
+            similarity_result.append(similarity(matches,kp2))
+            # Apply ratio test
+            # img3 = cv2.drawMatchesKnn(image1,kp1,image2,kp2,good,None,flags=2)
+            # plt.imshow(img3),plt.show()
+        else:
+            similarity_result.append('None')
+    df['similarity.result']=similarity_result
+    df.to_csv('result.csv',columns=['supplier.image','amazon.image','similarity.result'],index=0, header=1)
+  
+if __name__== "__main__":
+    main()
 
-image1 = cv2.cvtColor(image1,cv2.COLOR_BGR2GRAY)
-image2 = cv2.cvtColor(image2,cv2.COLOR_BGR2GRAY)
-
-# Initiate SIFT detector
-orb=cv2.ORB_create()
-
-#find the keypoints and descriptors with SIFT
-kp1, des1 =orb.detectAndCompute(image1,None)
-kp2, des2 =orb.detectAndCompute(image2,None)
-
-
-# create BFMatcher object
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-# Match descriptors.
-matches = bf.match(des1,des2)
-# Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
-# Draw first 10 matches.
-img3 = cv2.drawMatches(image1,kp1,image2,kp2,matches,None, flags=2)
-plt.imshow(img3),plt.show()
