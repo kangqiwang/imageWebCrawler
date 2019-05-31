8cad8fdedc2468d5edf8bf63da1357b87a42c188
kang wong <kang@sourcemogul.com>
2019-05-31 16:34:25 +0100

    add argos

---
+++ pagnition/supplier/www.wilko.com/pagination.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import requests
+
+
+
+def pagnition():
+    df=pd.read_csv("pagnition/input/wilko_com.csv",usecols=['Category URL","Category Name","First Page","Last Page","Source Domain","Product Count'])
+
+    tmpList=['url']
+    for url in df['Category URL","Category Name","First Page","Last Page","Source Domain","Product Count']:
+        urltmp=''
+        numbertmp=0
+        if "\",\"" in str(url):
+            urltmp = url.split("\",\"")[0]
+            numbertmp=url.split("\",\"")[5]
+
+        perpageCount=60
+        pageNum=int(int(numbertmp)/perpageCount)+1
+        for i in range(pageNum):
+
+            # if urltmp !='':
+            tmpurl = urltmp+'?page='+str(i)+'&pageSize=96'
+            # else:
+            #     tmpurl = url +'?Nao='+str(21*(i+1))+'&Ns=None&storeSelection=2408,2414,2409,2407,2404'
+            print(tmpurl)
+            tmpList.append(tmpurl)
+    savedf=pd.Series(tmpList)
+    savedf.to_csv("pagnition/output/wilko_com.csv",index=False)
+
+pagnition()
